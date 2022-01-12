4c44bc2e7020a640bebc9e9802e12aba0aee36ea
ZHI <891023883@qq.com>
2022-01-12 18:16:52 +0800

    引入OpenFeign实现服务接口调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/qinzhi/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,17 @@
+package com.qinzhi.springcloud.service;
+
+import com.qinzhi.springcloud.entities.CommonResult;
+import com.qinzhi.springcloud.entities.Payment;
+import feign.Param;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+}
