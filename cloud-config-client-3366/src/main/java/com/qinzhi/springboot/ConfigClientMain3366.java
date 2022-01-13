1b158428198a118cd18eb8f056d7cc16bebd748b
ZHI <891023883@qq.com>
2022-01-13 16:09:07 +0800

    引入RabbitMQ 实现SpringCloud Bus消息总线，动态更新配置

--- cloud-config-client-3366/src/main/java/com/qinzhi/springboot/ConfigClientMain3366.java
+++ cloud-config-client-3366/src/main/java/com/qinzhi/springboot/ConfigClientMain3366.java
@@ -1,4 +1,4 @@
-package com.qinzhi.springcloud;
+package com.qinzhi.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ConfigClientMain3355 {
+public class ConfigClientMain3366 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientMain3355.class, args);
+        SpringApplication.run(ConfigClientMain3366.class, args);
     }
 }
